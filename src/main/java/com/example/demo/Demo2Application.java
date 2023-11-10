118c39ebe8e5e611d66038fb0de7ee72f40ec2c8
amrmatar159 <aya.mounir167@gmail.com>
2023-11-10 11:16:57 +0200

    navigation

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -8,6 +8,6 @@ public class Demo2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo2Application.class, args);
-        System.out.println("-----MAin-----");
+//        System.out.println("-----MAin-----");
     }
 }
